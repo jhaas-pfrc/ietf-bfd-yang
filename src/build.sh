bf364de2866c76e4bc48a157638c7d9ab0a66b5f
Reshad Rahman <rrahman@cisco.com>
2017-06-22 18:00:14 -0500

    Minor changes in YANG files (addressing some comments) Updated yang files in dependencies Updated build.sh (YANG build script)

--- src/build.sh
+++ src/build.sh
@@ -17,3 +17,17 @@ pyang --ietf ietf-bfd-lag.yang -p dependencies/
 pyang --ietf ietf-bfd-mpls.yang -p dependencies/
 pyang --ietf ietf-bfd-mpls-te.yang -p dependencies/
 echo "pyang compile done"
+
+echo "confdc compile ietf-bfd.yang"
+confdc -c ietf-bfd.yang --yangpath dependencies
+echo "confdc compile ietf-bfd-ip-sh.yang"
+confdc -c ietf-bfd-ip-sh.yang --yangpath dependencies
+echo "confdc compile ietf-bfd-ip-mh.yang"
+confdc -c ietf-bfd-ip-mh.yang --yangpath dependencies
+echo "confdc compile ietf-bfd-lag.yang"
+confdc -c ietf-bfd-lag.yang --yangpath dependencies
+echo "confdc compile ietf-bfd-mpls.yang"
+confdc -c ietf-bfd-mpls.yang --yangpath dependencies
+echo "confdc compile ietf-bfd-mpls-te.yang"
+confdc -c ietf-bfd-mpls-te.yang --yangpath dependencies
+echo "confdc compile done"
