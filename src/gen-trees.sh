6aecebfd38def24ef779d8772e3d2ce8af175bd9
Mahesh Jethanandani <mjethanandani@gmail.com>
2018-02-26 13:25:29 -0800

    Fixed Makefile to use git tag and other fixes.

--- src/gen-trees.sh
+++ src/gen-trees.sh
@@ -1,8 +1,21 @@
-for i in yang/*-bfd*\@$(date +%Y-%m-%d).yang
+#!/bin/sh
+
+#
+# First check to see if you need to create/update your yang repository of
+# all IETF published YANG models.
+#
+# rsync -avz --delete rsync.ietf.org::iana/yang-parameters ../../
+
+for i in yang/*\@$(date +%Y-%m-%d).yang
 do
-	name=$(echo $i | cut -f 1 -d '.')
-	echo $name
-	pyang -p ../ -f tree --tree-line-length 69 $name.yang > $name-tree.txt.tmp
-	fold -w 71 $name-tree.txt.tmp > $name-tree.txt
+    name=$(echo $i | cut -f 1 -d '.')
+    echo $name
+    if test "${name#^example}" = "$name"; then
+        pyang -p ../../ -f tree --tree-line-length=69 $name.yang > $name-tree.txt.tmp
+    else
+        pyang --ietf -p ../../ -f tree --ietf --tree-line-length=69 $name.yang > $name-tree.txt.tmp
+    fi
+    fold -w 71 $name-tree.txt.tmp > $name-tree.txt
+    yanglint -p ../../ $name.yang
 done
 rm yang/*-tree.txt.tmp
